0da88d210ca8c33e0511a93c22cdec897e35b6b4
Admin <sestemika@gmail.com>
2021-08-04 16:43:01 +0300

    spring server 0.2 Internet hero. Google Auth

--- src/main/java/com/yainnixdev/springleaf/server/controller/GameController.java
+++ src/main/java/com/yainnixdev/springleaf/server/controller/GameController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @Controller
-@RequestMapping("/game")
+@MessageMapping("/game")
 public class GameController {
     @SendTo("/topic")
     @MessageMapping("/topic")
