4bc28c72fed3955cf647cd63a0075f0ae16c63f7
Admin <sestemika@gmail.com>
2021-08-04 16:37:20 +0300

    spring server 0.2 Internet hero. Google Auth

--- src/main/java/com/yainnixdev/springleaf/server/controller/GameController.java
+++ src/main/java/com/yainnixdev/springleaf/server/controller/GameController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/game")
 public class GameController {
     @SendTo("/topic")
+    @MessageMapping("/topic")
     public HeroDto playerPosition(@RequestBody HeroDto heroDto){
         System.out.println(heroDto.getMoveIntention().x);
         System.out.println(heroDto.getHero().getHeroName());
