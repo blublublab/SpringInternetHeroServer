0b4fd6a213a81babe851f5941b8e1c7b385acbb6
Sazhnyev Evhenii <sazhnyev.evhenii@fatoom.tech>
2021-11-15 21:12:16 +0200

    Spring server point save

--- src/main/java/com/yainnixdev/springleaf/server/repository/HeroDto.java
+++ src/main/java/com/yainnixdev/springleaf/server/repository/HeroDto.java
@@ -1,13 +1,17 @@
 package com.yainnixdev.springleaf.server.repository;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.yainnixdev.springleaf.server.utils.Point;
 import lombok.Data;
 
 @Data
 public class HeroDto {
     private Point moveIntention;
+
+    @JsonProperty("point")
     private Point heroCoordinates;
+
     private String heroName;
 
     public HeroDto(Point heroCoordinates, Point moveIntention, String heroName){
