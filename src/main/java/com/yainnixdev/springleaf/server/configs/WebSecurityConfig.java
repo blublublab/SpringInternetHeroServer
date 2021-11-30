1b529cd6b9dfbd29d8d4d8524775220f774d8eb7
Sazhnyev Evhenii <sazhnyev.evhenii@fatoom.tech>
2021-11-30 17:57:30 +0200

    Spring server point save

--- src/main/java/com/yainnixdev/springleaf/server/configs/WebSecurityConfig.java
+++ src/main/java/com/yainnixdev/springleaf/server/configs/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -46,11 +47,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .formLogin().permitAll()
-                    .loginPage("/login")
-                    .defaultSuccessUrl("/admin/main", true)
-                    .and()
-                    .logout().permitAll();
+                .formLogin()
+                    .loginPage("/login");
             /*        .and()
 
                 .addFilterBefore(loginFilter, UsernamePasswordAuthenticationFilter.class)
@@ -70,7 +68,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
