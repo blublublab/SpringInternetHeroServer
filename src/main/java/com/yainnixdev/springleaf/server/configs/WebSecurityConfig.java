8d6ac000d8f64c61b914889c100a18c06bb0f0c6
Sazhnyev Evhenii <sazhnyev.evhenii@fatoom.tech>
2021-11-30 03:59:21 +0200

    Spring server point save

--- src/main/java/com/yainnixdev/springleaf/server/configs/WebSecurityConfig.java
+++ src/main/java/com/yainnixdev/springleaf/server/configs/WebSecurityConfig.java
@@ -2,10 +2,12 @@ package com.yainnixdev.springleaf.server.configs;
 
 import com.yainnixdev.springleaf.server.service.UserService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
@@ -31,7 +33,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
-
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("12345").roles("ADMIN");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -40,7 +47,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                     .loginPage("/templates/login.html")
                     .loginProcessingUrl("/login")
-                    .defaultSuccessUrl("/admin.html", true)
+                    .defaultSuccessUrl("/admin/admin.html", true)
+
                     .permitAll()
                     .and()
                 .addFilterBefore(loginFilter, UsernamePasswordAuthenticationFilter.class)
