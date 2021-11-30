800b476e98211f30f233d9c08185cff5db9a73a0
Sazhnyev Evhenii <sazhnyev.evhenii@fatoom.tech>
2021-11-30 18:33:32 +0200

    Spring server point save

--- src/main/java/com/yainnixdev/springleaf/server/configs/WebSecurityConfig.java
+++ src/main/java/com/yainnixdev/springleaf/server/configs/WebSecurityConfig.java
@@ -1,27 +1,18 @@
 package com.yainnixdev.springleaf.server.configs;
 
-import com.yainnixdev.springleaf.server.domain.UserRole;
 import com.yainnixdev.springleaf.server.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
-import java.util.Collections;
 
 
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final DataSource dataSource;
     private final UserService userService;
