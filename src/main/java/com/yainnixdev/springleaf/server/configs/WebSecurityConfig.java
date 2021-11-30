fb4d9da234e206303ec371a2a673b6dc38f70106
Sazhnyev Evhenii <sazhnyev.evhenii@fatoom.tech>
2021-11-30 04:13:41 +0200

    Spring server point save

--- src/main/java/com/yainnixdev/springleaf/server/configs/WebSecurityConfig.java
+++ src/main/java/com/yainnixdev/springleaf/server/configs/WebSecurityConfig.java
@@ -49,8 +49,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                     .loginPage("/login")
                     .loginProcessingUrl("/login")
-                    .usernameParameter("username")
-                     .passwordParameter("password")
                     .defaultSuccessUrl("/admin", true)
                     .permitAll()
                     .and()
